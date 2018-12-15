c10ff8b2d322431bd9bcb18ea812a27efbdf6ad1
Andre Guerra <agu3rra@gmail.com>
2018-12-15 10:23:07 -0200

    version number updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pykeys',
-    version='18.12.0',
+    version='18.12.3',
     packages=find_packages(),
     install_requires=['cryptography'],
     author='Andre Guerra',
